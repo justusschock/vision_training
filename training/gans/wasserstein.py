3afc5864f91fc5b39e393a57b5490b21451e5036
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-17 10:12:48 +0200

    Add Trainscript for Wasserstein GAN

---
+++ training/gans/wasserstein.py
@@ -0,0 +1,20 @@
+from deliravision.models.gans import WassersteinGAN
+import os
+from training.gans._basic import train, predict
+import torch
+
+if __name__ == '__main__':
+    img_path = "~/data/"
+    outpath = "~/GanExperiments"
+    num_epochs = 1000
+    key_mapping = {"x": "data"}
+    torch.autograd.set_detect_anomaly(True)
+
+    model, weight_path = train(WassersteinGAN,
+                               {"latent_dim": 100,
+                                "img_shape": (1, 28, 28)},
+                               os.path.join(outpath, "train"), img_path,
+                               num_epochs=num_epochs,
+                               key_mapping=key_mapping)
+
+    predict(model, weight_path, os.path.join(outpath, "preds"), num_epochs)
