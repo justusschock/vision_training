9126d43a18245535cc624d3181c170dd260a1345
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-17 10:12:09 +0200

    Add Trainscript for Relativistic GAN

---
+++ training/gans/relativistic.py
@@ -0,0 +1,24 @@
+from deliravision.models.gans import RelativisticGAN
+from deliravision.losses import AdversarialLoss
+import os
+from training.gans._basic import train, predict
+import torch
+
+if __name__ == '__main__':
+    img_path = "~/data/"
+    outpath = "~/GanExperiments"
+    num_epochs = 1000
+    key_mapping = {"x": "data"}
+
+    model, weight_path = train(RelativisticGAN,
+                               {"latent_dim": 100,
+                                "img_size": 28,
+                                "num_channels": 1},
+                               os.path.join(outpath, "train"), img_path,
+                               num_epochs=num_epochs,
+                               key_mapping=key_mapping,
+                               additional_losses={
+                                   "adversarial": AdversarialLoss(
+                                       torch.nn.BCEWithLogitsLoss())})
+
+    predict(model, weight_path, os.path.join(outpath, "preds"), num_epochs)
