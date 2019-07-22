6d87d2cff4fed7c5fef2df8d70c03719ac0e47f9
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-22 10:51:14 +0200

    CHange Normalization to range norm

--- training/gans/_basic.py
+++ training/gans/_basic.py
@@ -7,7 +7,7 @@ from torchvision.utils import save_image
 import os
 from tqdm import tqdm
 from deliravision.losses import AdversarialLoss
-from batchgenerators.transforms import ZeroMeanUnitVarianceTransform
+from batchgenerators.transforms import RangeTransform
 
 
 def create_optims(model, optim_cls,
@@ -76,8 +76,8 @@ def train(model_cls, model_kwargs: dict, outpath: str, data_path, exp_name=None,
         }
     })
     data = setup_data(data_path, params.nested_get("batchsize"), 4,
-                      ZeroMeanUnitVarianceTransform(),
-                      ZeroMeanUnitVarianceTransform(),
+                      RangeTransform(),
+                      RangeTransform(),
                       dset_type)
     exp = PyTorchExperiment(params, model_cls,
                             params.nested_get("num_epochs"),
